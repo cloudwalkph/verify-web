b31bc4cb9d54425420f2c0d3cad10b84ddbf99b0
Alleo Indong <freedom_war550@live.com.ph>
2017-03-21 00:52:27 +0800

    updated home style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="alert alert-primary" style="text-align: center">
+    <img src="{{ asset('images/ic_sms_failed_24px.png') }}" alt="info"> CLICK ON A PROJECT TO VIEW MORE DETAILS
+</div>
 
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
                 <div class="panel-body">
-                    You are logged in!
+
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Project Name</th>
+                                <th>Active Runs</th>
+                                <th>Achieved Target Hits</th>
+                                <th>Deployed Runs</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <strong>Creamsilk Hair Dare</strong>
+                                </td>
+
+                                <td>
+                                    0 / 100
+                                </td>
+
+                                <td>
+                                    2500 / 5000
+                                </td>
+
+                                <td>
+                                    500 / 1000
+                                </td>
+
+                                <td>
+                                    Active
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <strong>Creamsilk Hair Dare</strong>
+                                </td>
+
+                                <td>
+                                    0 / 100
+                                </td>
+
+                                <td>
+                                    2500 / 5000
+                                </td>
+
+                                <td>
+                                    500 / 1000
+                                </td>
+
+                                <td>
+                                    Active
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <strong>Creamsilk Hair Dare</strong>
+                                </td>
+
+                                <td>
+                                    0 / 100
+                                </td>
+
+                                <td>
+                                    2500 / 5000
+                                </td>
+
+                                <td>
+                                    500 / 1000
+                                </td>
+
+                                <td>
+                                    Active
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
