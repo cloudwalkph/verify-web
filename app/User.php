f6f6b0503ddc167ca559377a490b281f509b1a00
Alleo Indong <freedom_war550@live.com.ph>
2017-03-20 21:27:35 +0800

    added more packages and auth passport

--- app/User.php
+++ app/User.php
@@ -2,21 +2,23 @@
 
 namespace App;
 
+use App\Models\Project;
+use App\Models\UserGroup;
+use App\Models\UserProfile;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -26,4 +28,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function group()
+    {
+        return $this->belongsTo(UserGroup::class);
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
 }
