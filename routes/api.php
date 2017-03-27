6ee170aedcefd4320abf9418bf18b175e2ba8925
Alleo Indong <freedom_war550@live.com.ph>
2017-03-27 15:22:33 +0800

    added events api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1',
+    'middleware' => 'auth:api',
+    'namespace' => 'Auth'], function() {
+
+    Route::get('events', 'EventsController@self');
+});
