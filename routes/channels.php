eb7a2bab66b1d865d14fc62f82eb7a6bbd0cfc9b
Alleo Indong <freedom_war550@live.com.ph>
2017-03-28 07:32:56 +0800

    added socket channel

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,17 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('location.{locationId}', function ($user, $locationId) {
+    $location = \App\Models\ProjectLocation::where('id', $locationId)->first();
+
+    if (! $location) {
+        return false;
+    }
+
+    $project = \App\Models\Project::where('id', $location->project_id)
+        ->where('user_id', $user->id)
+        ->first();
+
+    return $project;
+});
