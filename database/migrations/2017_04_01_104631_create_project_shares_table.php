75fb5fc169fea9f7d2c2bab614711415393dd5df
Alleo Indong <freedom_war550@live.com.ph>
2017-04-01 11:01:55 +0800

    updated project shares migration

--- database/migrations/2017_04_01_104631_create_project_shares_table.php
+++ database/migrations/2017_04_01_104631_create_project_shares_table.php
@@ -15,7 +15,11 @@ class CreateProjectSharesTable extends Migration
     {
         Schema::create('project_shares', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->enum('status', ['active', 'revoked'])->default('active');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
