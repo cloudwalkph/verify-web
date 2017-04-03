0844e11dd311e86710f42cca177f3c6f87867695
Alleo Indong <freedom_war550@live.com.ph>
2017-04-03 22:02:44 +0800

    updated

--- app/Http/Controllers/EventsReportController.php
+++ app/Http/Controllers/EventsReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Hit;
 use App\Models\Project;
 use App\Models\ProjectLocation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class EventsReportController extends Controller
