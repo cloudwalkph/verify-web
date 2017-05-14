4c0831e3f7f63bc5239a215922989aa8f089415b
Alleo Indong <freedom_war550@live.com.ph>
2017-05-15 00:09:23 +0800

    updated front page of verify admin

--- app/Http/Controllers/Management/ProjectsController.php
+++ app/Http/Controllers/Management/ProjectsController.php
@@ -2,11 +2,14 @@
 namespace App\Http\Controllers\Management;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 
 class ProjectsController extends Controller
 {
     public function index()
     {
-        echo 'management';
+        $projects = Project::all();
+
+        return view('management.index', compact('projects'));
     }
 }
---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="alert alert-primary" style="text-align: center">
+        <img src="{{ asset('images/ic_sms_failed_24px.png') }}" alt="info"> CLICK ON A PROJECT TO VIEW MORE DETAILS
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h1 class="agency-title">Activations Advertising Inc</h1>
+                        <h4>Monitor all the data from consumer engagement activities</h4>
+
+                        <legend>My Projects</legend>
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>Project Name</th>
+                                <th>Active Runs</th>
+                                <th>Achieve Sampling Target Hits</th>
+                                <th>Completed Runs</th>
+                                <th>Status</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            @foreach ($projects as $project)
+                                <tr class="clickable" data-uri="/projects/{{ $project->id }}">
+                                    <td>
+                                        <strong>{{ $project->name }}</strong>
+                                    </td>
+
+                                    <td>
+                                        {{ $project->locations()->onGoing()->count() }} / {{ $project->locations()->total() }}
+                                    </td>
+
+                                    <td>
+                                        {{ get_total_hits_for_project($project->locations) > $project->locations()->sum('target_hits') ? $project->locations()->sum('target_hits') : get_total_hits_for_project($project->locations) }} / {{ $project->locations()->sum('target_hits') }}
+                                    </td>
+
+                                    <td>
+                                        {{ $project->locations()->completed()->count() }} / {{ $project->locations()->total() }}
+                                    </td>
+
+                                    <td>
+                                        {{ ucwords($project->status) }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
