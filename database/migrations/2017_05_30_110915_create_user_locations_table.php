8e009a87697a6f8a5d0e08e7a16f6a2501cb0551
Alleo Indong <freedom_war550@live.com.ph>
2017-05-30 11:11:41 +0800

    added user location

---
+++ database/migrations/2017_05_30_110915_create_user_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('project_location_id')->unsigned();
+            $table->double('lat');
+            $table->double('lng');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_locations');
+    }
+}
