748e7f7ce0ef74c95c85a738b7717a4323eec276
Alleo Indong <freedom_war550@live.com.ph>
2017-07-10 14:13:01 +0800

    updated dialog

--- resources/views/projects/locations/show.blade.php
+++ resources/views/projects/locations/show.blade.php
@@ -25,7 +25,7 @@
             </div>
 
             <div class="modal fade" id="uploadImages" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog" style="width: 80%" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
