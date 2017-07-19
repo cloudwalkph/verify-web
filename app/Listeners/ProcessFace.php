c300d9ff0b620574b712f218461ee92af3dcec80
Alleo Indong <freedom_war550@live.com.ph>
2017-07-20 04:38:32 +0800

    face processing

--- app/Listeners/ProcessFace.php
+++ app/Listeners/ProcessFace.php
@@ -8,9 +8,12 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Carbon\Carbon;
 use App\Models\Hit;
 use Aws\Rekognition\RekognitionClient;
+use Aws\Rekognition\Exception\RekognitionException;
 
 class ProcessFace implements ShouldQueue
 {
+    protected $rekog;
+
     /**
      * Create the event listener.
      *
@@ -18,7 +21,12 @@ class ProcessFace implements ShouldQueue
      */
     public function __construct()
     {
-        //
+        $args = [
+            'region'  => 'us-east-1',
+            'version'   => 'latest'
+        ];
+
+        $this->rekog = new RekognitionClient($args);
     }
 
     /**
@@ -29,36 +37,72 @@ class ProcessFace implements ShouldQueue
      */
     public function handle(NewFaceUploaded $event)
     {
+        $collection = $this->findOrCreateCollection($event->locationId);
+
         $this->processImage($event->path, $event->locationId, 
             $event->timestamp, $event->userId);
     }
 
+    private function findOrCreateCollection($locationId) 
+    {
+        $collectionName = 'collection-'.$locationId;
+
+        try {
+            $result = $this->rekog->createCollection([
+                'CollectionId'  => $collectionName
+            ]);
+
+            return $result;
+        } catch (RekognitionException $e) {
+            \Log::info('error on getting collection');
+
+            return null;
+        }
+    }
+
+    private function hasMatch($face, $collectionName) {
+        $result = $this->rekog->searchFaces([
+            'CollectionId' => $collectionName,
+            'FaceId' => $face['Face']['FaceId'],
+            'FaceMatchThreshold' => 80,
+            'MaxFaces' => 10,
+        ]);
+
+        \Log::info($result);
+
+        return count($result->get('FaceMatches')) <= 0 ? false : true;
+    }
+
     private function processImage($path, $locationId, $timestamp, $userId)
     {
+        $collectionName = 'collection-'.$locationId;
         $file = \Storage::drive('s3')->get($path);
 
-        $args = [
-            'region'  => 'us-east-1',
-            'version'   => 'latest'
-        ];
-
-        $rekog = new RekognitionClient($args);
-        $result = $rekog->detectFaces([
+        $result = $this->rekog->indexFaces([
+            'CollectionId'  => $collectionName,
+            'ExternalImageId'   => uniqid(),
             'Image' => [
                 'Bytes' => $file
             ],
-            'Attributes' => ['ALL']
+            'DetectionAttributes' => ['ALL']
         ]);
 
-        $faceDetails = $result->get('FaceDetails');
+        $faceDetails = $result->get('FaceRecords');
         if ($result->count() <= 0) {
             return;
         }
 
+        \Log::info($result);
+
         $hits = [];
         foreach ($faceDetails as $face) {
-            $ageRange = $face['AgeRange'];
-            $gender = $face['Gender'];
+            if ($this->hasMatch($face, $collectionName)) {
+                continue;
+            }
+
+            $faceDetail = $face['FaceDetail'];
+            $ageRange = $faceDetail['AgeRange'];
+            $gender = $faceDetail['Gender'];
 
             // Create a hit
             $hit = Hit::create([
