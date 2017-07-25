60a7d1a3c3525649f2b9fc65a05281a2d1d1a7d4
Alleo Indong <freedom_war550@live.com.ph>
2017-07-26 06:58:03 +0800

    updated accounts

--- resources/views/management/admin/accounts/index.blade.php
+++ resources/views/management/admin/accounts/index.blade.php
@@ -25,41 +25,38 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <div class="active tab-pane" id="profile">
-                            <div class="content">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <h1>Users</h1>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <a href="/management/users/create" class="btn btn-primary pull-right" style="width: 200px; margin-top: 25px">Create New Account</a>
-                                    </div>
-                                </div>
-
-                                <hr>
-
-                                 <table id="users" class="table table-bordered table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>User Group</th>
-                                        <th>Email Address</th>
-                                        <th>Date</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($users as $user)
-                                            <tr class="clickable" data-uri="/management/users/update/{{ $user->id }}">
-                                                <td>{{ $user->profile->first_name }} {{ $user->profile->last_name }}</td>
-                                                <td>{{ $user->userGroup->name }}</td>
-                                                <td>{{ $user->email }}</td>
-                                                <td>{{ $user->created_at->toFormattedDateString() }}</td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h1 class="agency-title">Users</h1>
+                                <h4>Manage all users</h4>
+                            </div>
+                            <div class="col-md-6">
+                                <a href="/management/users/create" class="btn btn-primary pull-right" style="width: 200px; margin-top: 25px">Create New Account</a>
                             </div>
                         </div>
+
+                        <hr>
+
+                        <table id="users" class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>User Group</th>
+                                <th>Email Address</th>
+                                <th>Date</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr class="clickable" data-uri="/management/users/update/{{ $user->id }}">
+                                    <td>{{ $user->profile->first_name }} {{ $user->profile->last_name }}</td>
+                                    <td>{{ $user->userGroup->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->created_at->toFormattedDateString() }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
