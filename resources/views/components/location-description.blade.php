60f012ce57ac4cc7c707e602c3d6add76bf12233
Niña Kristel M. Cheng <nkmcheng@live.com>
2017-08-07 14:01:31 +0800

    update

--- resources/views/components/location-description.blade.php
+++ resources/views/components/location-description.blade.php
@@ -1,7 +1,7 @@
 <div class="black-description project-location">
     <div class="col-md-4 col-sm-6 col-xs-12">
         <h4><b>Type:</b> {{ ($location->project_type) ? $location->project_type : 'NA' }} </h4>
-        <h4><b>Target Hits:</b> {{ ($location->target_hits > 0) ? $location->target_hits.' Hits' : 'No target hits' }} </h4>
+        <h4><b>Reported Hits / Target Hits:</b> {{ ($location->manual_hits > 0) ? $location->manual_hits : 'NA' }} / {{ ($location->target_hits > 0) ? $location->target_hits : 'NA' }} </h4>
     </div>
     <div class="col-md-4 col-sm-6 col-xs-12">
         <h4><b>Run Date:</b> {{ $location->date }} </h4>
