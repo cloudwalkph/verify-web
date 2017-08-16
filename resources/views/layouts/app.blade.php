835a87f343b52c60fc47f18f3ffbf52d490c3103
Alleo Indong <freedom_war550@live.com.ph>
2017-08-17 00:56:55 +0800

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,9 +66,7 @@
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="/management/profile">Profile</a>
-                                    </li>
+
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
