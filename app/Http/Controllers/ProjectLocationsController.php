07f2c24822b3ab4568e8c80e6d547f11e664d0c6
Alleo Indong <freedom_war550@live.com.ph>
2017-10-11 00:44:00 +0800

    updated

--- app/Http/Controllers/ProjectLocationsController.php
+++ app/Http/Controllers/ProjectLocationsController.php
@@ -171,7 +171,11 @@ class ProjectLocationsController extends Controller
 
             $filename = uniqid() . '-' . $locationId . '-' . $file->getClientOriginalName();
 //            $path = \Storage::drive('s3')->putFileAs('videos/'.$projectId.'/'.$locationId, $file, $filename, 'public');
-            $path = \Storage::disk('local')->putFileAs('videos', $file, $filename, 'public');
+//            $path = \Storage::disk('local')->putFileAs('videos', $file, $filename, 'public');
+            $path = $request->file('file')->storeAs('videos', $filename, [
+                'disk'  => 'local',
+                'visibility'   => 'public'
+            ]);
 
             $timestamp = $request->has('hit_timestamp') ? $request->get('hit_timestamp') : Carbon::today()->toDateTimeString();
 
