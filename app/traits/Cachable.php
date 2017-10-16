d3ed639d01f2b18dd9a36da9f9ccecf281a110a3
Alleo Indong <freedom_war550@live.com.ph>
2017-10-16 19:34:13 +0800

    updated cache

--- app/traits/Cachable.php
+++ app/traits/Cachable.php
@@ -38,7 +38,7 @@ trait Cachable {
     public function addToCache($key, $data)
     {
         if (\Cache::has($key)) {
-            $collection = \Cache::get('$key');
+            $collection = \Cache::get($key);
             $collection->push($data);
 
             \Cache::forever($key, $collection);
