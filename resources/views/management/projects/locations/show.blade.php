684985bf9e5700f64e54dde0b7424e6d645a8d2e
Alleo Indong <freedom_war550@live.com.ph>
2017-10-26 18:30:51 +0800

    updated video upload size

--- resources/views/management/projects/locations/show.blade.php
+++ resources/views/management/projects/locations/show.blade.php
@@ -214,7 +214,7 @@
 
         Dropzone.options.myDropzoneVideos = {
             paramName: "file", // The name that will be used to transfer the file
-            maxFilesize: 1024, // MB
+            maxFilesize: 3000, // MB
             acceptedFiles: 'video/*, .mkv',
             timeout: 600000,
             accept: function(file, done) {
