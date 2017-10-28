823618a532ae4dfba24a593b71501732afcd20f8
Alleo Indong <freedom_war550@live.com.ph>
2017-10-29 01:34:08 +0800

    added migration

---
+++ database/migrations/2017_10_29_013201_add_status_field_on_raw_videos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddStatusFieldOnRawVideos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('raw_videos', function(Blueprint $table) {
+            $table->enum('status', ['pending', 'completed', 'cancelled']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('raw_videos', function(Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
