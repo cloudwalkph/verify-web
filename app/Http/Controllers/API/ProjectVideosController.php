13e47ded773ddad7248d60e06b85a48e630fcbb3
Alleo Indong <freedom_war550@live.com.ph>
2017-10-29 11:49:07 +0800

    updated

--- app/Http/Controllers/API/ProjectVideosController.php
+++ app/Http/Controllers/API/ProjectVideosController.php
@@ -32,7 +32,7 @@ class ProjectVideosController extends Controller {
             // Create result
             $video->results()->create([
                 'file'      => $request->get('file_name'),
-                'result'    => $request->get('result'),
+                'result'    => json_encode($request->get('result')),
                 'face_id'   => $request->get('face_id')
             ]);
 
