469189dae8463ab6c9ee22cd8d67a82acefeeeea
Niña Kristel M. Cheng <nkmcheng@live.com>
2017-10-30 12:20:55 +0800

    update verified hits

--- resources/views/management/projects/locations/show-automated.blade.php
+++ resources/views/management/projects/locations/show-automated.blade.php
@@ -116,9 +116,9 @@
 
 
     @component('components.location-description',
-        ['project' => $project, 'location' => $location])
+        ['project' => $project, 'location' => $location, 'chartData' => $chartData])
     @endcomponent
-
+{
     <div class="container-fluid" style="margin-top: -130px">
         <div class="row">
             <div class="col-md-12">
